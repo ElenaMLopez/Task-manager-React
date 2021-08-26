8b5e1bf45bcdcb38ed96db2dff65e6af45d6ac94
elenamlopez <elena.mateos1@gmail.com>
2021-08-26 12:08:24 +0200

    Add the first approach for a state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 
 function App() {
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      text: 'Learn React',
+      day: '26 August 2021',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Learn Redux',
+      day: '29 August 2021',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Buy led switches',
+      day: '26 August 2021',
+      reminder: true,
+    },
+  ])
 
   return (
     <div className="container">
      <Header />
-     <Tasks />
+     <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,11 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Learn React',
-    day: '26 August 2021',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Learn Redux',
-    day: '29 August 2021',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Buy led switches',
-    day: '26 August 2021',
-    reminder: true,
-  },
-];
 
-const Tasks = () => {
+const Tasks = ({tasks}) => {
+  
   return (
     <>
-      {tasks.map((task) => <h3 key={task.id}>{task.text}</h3>)}
+      {tasks.map((task) => (
+      <h3 key={task.id}>{task.text}</h3>
+      ))}
     </>
   )
 }
